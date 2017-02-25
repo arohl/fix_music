ad1eca97c166176a634903c51faf2a15499cdffa
Andrew Rohl <a.rohl@curtin.edu.au>
2017-02-25 11:54:07 +0800

    First pass at providing complete functionality

--- fix_metadata.rb
+++ fix_metadata.rb
@@ -4,7 +4,7 @@ require 'taglib'
 require 'trollop'
 
 opts = Trollop::options do
-  version "fix_metadata 0.0.1 (c) 2017 Andrew Rohl"
+  version "fix_metadata 1.0.0 (c) 2017 Andrew Rohl"
 banner <<-EOS
 fix_music_metadata is a program for easily changing the metadata of your music.
 
@@ -15,20 +15,11 @@ EOS
 
   opt :strip_leading_brackets, "Removes brackets from song title if first character", :short => 's'
   opt :artist, "Set the artist", :type => :string, :short => 'a'
+  opt :title, "Set the title", :type => :string, :short => 't'
   opt :genre, "Set the genre", :type => :string, :short => 'g'
   opt :year, "Set the year", :type => :int, :short => 'y'
 end
 
-p opts
-
-if opts[:strip_leading_brackets]
-  puts "stripping"
-end
-
-if opts[:year_given]
-  year = opts[:year]
-  puts "setting year to " + year.to_s
-end
 puts ""
 puts "| Track | Filename | Title | Artist | Album | Year | Genre | Comment |"
 puts "| ----- | -------- |------ | ------ | ----- | ---- | ----- | ------- |"
@@ -37,13 +28,54 @@ ARGV.each do |filename|
   TagLib::FileRef.open(filename) do |fileref|
     unless fileref.null?
       tag = fileref.tag
-      puts "| " + tag.track.to_s + " | " + tag.title + " | " + filename + " | " + tag.artist + " | " + tag.album + " | " + tag.year.to_s + " | " + tag.genre + " | " + tag.comment + " |"
 
-        new_title = tag.title
-      # remove any whitespace
-      new_title = tag.title.strip
+      if opts[:strip_leading_brackets]
+        count = 0
+        new_string = ""
+        # remove any whitespace
+        tmp_title = tag.title.strip
+        # does the title start with open bracket?
+        if tmp_title[0] == "("
+          # if so, iterate over rest of string
+          tmp_title[1..-1].each_char do |c|
+            copy_c = true
+            if c == "("
+              count += 1
+            elsif c == ")"
+              if count == 0
+                copy_c = false
+              else
+                count -= 1
+              end
+            end
+            if copy_c
+              new_string << c
+            end
+          end
+          tag.title = new_string
+        end
+      end
 
-  #    fileref.save
+      if opts[:artist_given]
+        tag.artist = opts[:artist]
+      end
+
+      if opts[:title_given]
+        tag.title = opts[:title]
+      end
+
+      if opts[:genre_given]
+        tag.genre = opts[:genre]
+      end
+
+      if opts[:year_given]
+        tag.year = opts[:year]
+      end
+
+      fileref.save
+
+      puts "| " + tag.track.to_s + " | " + filename + " | " + tag.title + " | " + tag.artist + " | " + tag.album + " | " + tag.year.to_s + " | " + tag.genre + " | " + tag.comment + " |"
+
     end
   end
 end
